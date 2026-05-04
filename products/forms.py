395440b8bb5aca396700e42a4b7c7f3a4091a7e1
maurojasaros <114428046+maurojasaros@users.noreply.github.com>
2026-05-03 20:20:11 -0400

    Actualización de errores todavia

--- products/forms.py
+++ products/forms.py
@@ -39,6 +39,15 @@ class RegisterForm(UserCreationForm):
             'placeholder': 'Confirmar contraseña'
         })
 
+    def clean_username(self):
+        username = self.cleaned_data.get('username')
+
+        if User.objects.filter(username=username).exists():
+            raise forms.ValidationError("Este nombre de usuario ya está en uso")
+
+        return username
+
+
     # VALIDACIÓN EMAIL ÚNICO
     def clean_email(self):
         email = self.cleaned_data.get('email')
