d8b65a217441fc13ac04b691212778176389553a
maurojasaros <114428046+maurojasaros@users.noreply.github.com>
2026-05-08 00:22:06 -0400

    Integrando Supabase Storage para imagenes y pdfs

--- products/models.py
+++ products/models.py
@@ -24,11 +24,11 @@ class Product(models.Model):
 
     categoria = models.ForeignKey(Category, on_delete=models.CASCADE)
 
-    imagen = models.ImageField(upload_to='productos/', null=True, blank=True)
+    imagen = models.URLField(blank=True, null=True)
 
     # CASA
     m2 = models.IntegerField(null=True, blank=True)
-    planos = models.FileField(upload_to='planos/', null=True, blank=True)
+    planos = models.URLField(blank=True, null=True)
     ubicacion = models.CharField(max_length=100, null=True, blank=True)
 
     # MUEBLE
